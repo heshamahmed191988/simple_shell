66f4aecb6a79ae36b829011f4d6be889c9be3cdf
heshamahmed191988 <heshamahmed191988@gmail.com>
2023-05-24 00:40:59 -0700

    l

---
+++ taskfive.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/* check if the command is a built-in command and execute it if so */
+int builtin_command(char **argv)
+{
+    if (!strcmp(argv[0], "exit")) { /* exit command */
+        exit(0);
+    }
+    if (!strcmp(argv[0], "env")) { /* env command */
+        extern char **environ; /* pointer to environment variables array */
+        int i = 0;
+        while (environ[i]) { /* print each environment variable on a new line */
+            printf("%s\n", environ[i++]);
+        }
+        return 1;
+    }
+    
+    return 0; /* not a built-in command */
+}
